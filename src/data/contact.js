106838e3fa9b7abe410a7a7b0b47c46fb96bff97
ayush patwal <137012714+ayushpatwal011@users.noreply.github.com>
2025-07-16 22:04:06 +0530

    Update contact.js

--- src/data/contact.js
+++ src/data/contact.js
@@ -5,6 +5,6 @@ export const contact = {
         mail : "patwalayush2005@gmail.com",
         insta : "https://www.instagram.com/ayushpatwal7/?utm_source=qr&igsh=Y3VyM2JwY3VxcGhs#",
         linkedin : "https://www.linkedin.com/in/ayush-patwal-8618a1274?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-        whatsapp : "https://wa.me/qr/HS2Y3UQBGWZTJ1 "
+        whatsapp : "https://wa.me/qr/HS2Y3UQBGWZTJ1 ",
     }
 }
